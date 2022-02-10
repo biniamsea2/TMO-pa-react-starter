332763424bbea69b9c71694190fc2e04902d36a6
Biniam Tesfamariam <Biniamt23@yahoo.com>
2022-02-09 20:07:27 -0800

    working on error

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -4,7 +4,8 @@ function DisplayData({ inputData }) {
       {inputData.map((data, index) => {
         return (
           <li key={index}>
-            {data["recipe-name"]} {data["recipe-instructions"]}
+            {data["recipe-name"]} 
+            {/* {data["recipe-instructions"]} */}
           </li>
         );
       })}
