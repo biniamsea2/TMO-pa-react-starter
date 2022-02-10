55304a0957fe495b997a98051b54b2f67c88abf1
Biniam Tesfamariam <Biniamt23@yahoo.com>
2022-02-09 17:42:41 -0800

    trying to get errors to pass

--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,23 @@
 function Form({ handleChange, formInputData, handleSubmit }) {
   return (
     <form>
-        <label for="/recipe-name/i">recipe-name </label>
-        <input
-          name="/recipe-name/i"
-          // value={formInputData["recipe-name"]}
-          type="text"
-          onChange={handleChange}
-          placeholder="recipe-name"
-        />
-        <label for="/recipe-instructions/i">recipe-instructions </label>
-        <input
-          type="text"
-          onChange={handleChange}
-          // value={formInputData["recipe-instructions"]}
-          name="/recipe-instructions/i"
-          placeholder="recipe-instructions"
-        />
+      <label for="recipe-name">recipe-name </label>
+      <input
+        name="/recipe-name/i"
+        // value={formInputData["/recipe-name/i"]}
+        type="text"
+        onChange={handleChange}
+        placeholder="recipe-name"
+      />
+
+      <label for="recipe-instructions">recipe-instructions </label>
+      <input
+        type="text"
+        onChange={handleChange}
+        // value={formInputData["r/ecipe-instructions/i"]}
+        name="/recipe-instructions/i"
+        placeholder="recipe-instructions"
+      />
       <div>
         <button type="submit" onClick={handleSubmit}>
           Add
