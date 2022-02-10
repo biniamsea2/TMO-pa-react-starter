2c614919849de0b2c78d44f00aa0176186ae64b0
biniamsea2 <biniamt23@yahoo.com>
2022-02-10 10:39:48 -0800

    fixed warning issue

--- src/DisplayForm.js
+++ src/DisplayForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Form from "./Form";
-import DisplayData from "./DisplayData";
+// import Form from "./Form";
+// import DisplayData from "./DisplayData";
 
 function DisplayForm() {
   const [inputData, setInputData] = useState([]);
